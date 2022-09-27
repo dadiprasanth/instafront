d27e65d3844913da9553d6fb8c16e7b10704f614
dadiprasanth <dadiprasanth24@gmail.com>
2022-09-27 19:54:26 +0530

    something is added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import{BrowserRouter,Route,Routes}from "react-router-dom"
+import Upload from './componenst/upload';
+ 
+import LandingPage from './landingpage/landingpage';
+import PostView from './postview/postview';
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path='/postview' element={<PostView/>}/>
+        <Route path="/postview/upload" element={<Upload/>}/>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
-
+ 
 export default App;
